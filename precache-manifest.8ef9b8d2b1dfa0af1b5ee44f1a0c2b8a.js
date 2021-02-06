629a7a5af2fdb993ad68cd42cf44e45ff776b7c9
Souvik Nath <souviknsl07@gmail.com>
2021-02-06 22:36:11 +0530

    Updates

---
+++ precache-manifest.8ef9b8d2b1dfa0af1b5ee44f1a0c2b8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87032fbe9a7c01970c9aeb07e71d7f65",
+    "url": "/Robofriendapp/index.html"
+  },
+  {
+    "revision": "41fa906cec7c0daede29",
+    "url": "/Robofriendapp/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "0bd6eca8fe481fe3cbc5",
+    "url": "/Robofriendapp/static/css/main.6fcc2342.chunk.css"
+  },
+  {
+    "revision": "41fa906cec7c0daede29",
+    "url": "/Robofriendapp/static/js/2.ba3d2b0c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Robofriendapp/static/js/2.ba3d2b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd6eca8fe481fe3cbc5",
+    "url": "/Robofriendapp/static/js/main.4a14c12b.chunk.js"
+  },
+  {
+    "revision": "206f2552776dfd1f3150",
+    "url": "/Robofriendapp/static/js/runtime-main.706e9507.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriendapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
